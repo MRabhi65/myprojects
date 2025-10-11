79b48732c14486c502de1b4f4d85e7361ba277ff
MRabhi65 <abhishekmuz8581@gmail.com>
2025-10-11 06:56:24 +0530

    vegitabel website

---
+++ vegitabel website/main.js
@@ -0,0 +1,10 @@
+
+var swiper = new Swiper(".swiper-home", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
